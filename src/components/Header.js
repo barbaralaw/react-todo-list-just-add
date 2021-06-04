b74ded323e3f99720a97959ddd32c6d61169b03b
Barbara <64677474+barbaralaw@users.noreply.github.com>
2021-06-03 17:51:18 -0700

    commenting for understanding

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
+// import react from react
 import React from "react";
 
+// creates Header component to export, takes in any existing props
 const Header = (props) => {
+  // returns the following
   return (
+    // an html div element
     <div>
+      // an h1 element with styles from headerStyle object properties. Contains the title prop passed in
       <h1 style={headerStyle}>{props.title}</h1>
     </div>
   );
 };
 
+// create headerStyle object to be used on the h1
 const headerStyle = {
   fontSize: "40px",
   textDecoration: "underline",
 };
 
+// exports header component
 export default Header;
